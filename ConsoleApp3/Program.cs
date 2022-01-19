492419469235375e6fd70b08e4560b8d47924427
Виктор Редькин <Moridin830@gmail.com>
2022-01-20 00:01:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+//using Telegram.Bot.Extensions.Polling;
+
+namespace SkillFactory_Module11
+{
+
+    public static class Program
+    {
+
+        public static ITelegramBotClient botClient;
+
+        public static void Main()
+        {
+            botClient = new TelegramBotClient(BotCredentials.BotToken);
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+
+            Console.WriteLine("Нажмите любую кнопку для остановки");
+            Console.ReadKey();
+
+            botClient.StopReceiving();
+        }
+
+        private static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Получено сообщение в чате: {e.Message.Chat.Id}.");
+
+                await botClient.SendTextMessageAsync(
+                chatId: e.Message.Chat, text: "Вы написали:\n" + e.Message.Text);
+            }
+        }
+    }
+}
